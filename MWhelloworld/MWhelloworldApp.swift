60bcba0bd281509696cdda1b995b9d1415c89ef6
test <x@y.com>
2026-01-16 16:11:46 -0800

    first commit

---
+++ MWhelloworld/MWhelloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWhelloworldApp.swift
+//  MWhelloworld
+//
+//  Created by haroon shah on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MWhelloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
